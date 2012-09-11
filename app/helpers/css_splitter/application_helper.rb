4a9bf50fefb743edb738188fae7fac59138bb5b7
Jakob Hilden <jakobhilden@gmail.com>
2012-09-11 11:14:51 +0200

    added split_stylesheet_link_tag helper method

--- app/helpers/css_splitter/application_helper.rb
+++ app/helpers/css_splitter/application_helper.rb
@@ -1,7 +1,15 @@
 module CssSplitter
   module ApplicationHelper
-    def stylesheet_link_tag(*sources)
-      debugger
+    def split_stylesheet_link_tag(*sources)
+      forwarded_sources = []
+
+      sources.each do |source|
+        puts source.class
+        forwarded_sources << source
+        forwarded_sources << "#{source}_part2"  if [String, Symbol].include? source.class
+      end
+
+      stylesheet_link_tag(*forwarded_sources)
     end
   end
 end
---
+++ test/unit/helpers/css_splitter/application_helper_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+module CssSplitter
+  class ApplicationHelperTest < ActionView::TestCase
+    test "should work" do
+      #split_stylesheet_link_tag(:too_big_stylesheet)
+    end
+  end
+end
