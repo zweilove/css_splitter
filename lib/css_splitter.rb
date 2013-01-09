827d15f98824f76299aefb3ee505f0eea9ec4e21
Aaron Jensen <aaronjensen@gmail.com>
2013-01-08 17:25:59 -0800

    Make the sprocket engine addition an initializer
    This way it works even if you don't have initialize_on_precompile set

--- lib/css_splitter.rb
+++ lib/css_splitter.rb
@@ -1,7 +1,6 @@
 require "css_splitter/engine"
 require "css_splitter/sprockets_engine"
 require "css_splitter/splitter"
-require "css_splitter/railtie"
 
 module CssSplitter
 end
--- lib/css_splitter/engine.rb
+++ lib/css_splitter/engine.rb
@@ -2,6 +2,10 @@ module CssSplitter
   class Engine < ::Rails::Engine
     isolate_namespace CssSplitter
 
+    initializer 'css_splitter.sprockets_engine', after: 'sprockets.environment', group: :all do |app|
+      app.assets.register_engine '.split2', CssSplitter::SprocketsEngine
+    end
+
     initializer 'css_splitter.action_controller' do |app|
       ActiveSupport.on_load :action_controller do
         helper CssSplitter::ApplicationHelper
