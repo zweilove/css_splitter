e65a4fb45fb9ba31551e3593b63d4e528a8c9cb9
Jakob Hilden <jakobhilden@gmail.com>
2014-04-18 18:24:37 +0200

    updated dummy app to rails 4.1

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -3,7 +3,6 @@ require File.expand_path('../boot', __FILE__)
 # require 'rails/all'
 require "action_controller/railtie"
 require "action_mailer/railtie"
-require "active_resource/railtie"
 require "rails/test_unit/railtie"
 require "sprockets/railtie"
 
--- test/unit/helpers/css_splitter/application_helper_test.rb
+++ test/unit/helpers/css_splitter/application_helper_test.rb
@@ -5,12 +5,12 @@ module CssSplitter
 
     test "should work w/out options" do
       output = split_stylesheet_link_tag("too_big_stylesheet")
-      assert_equal "<link href=\"/stylesheets/too_big_stylesheet.css\" media=\"screen\" rel=\"stylesheet\" type=\"text/css\" />\n<!--[if lte IE 9]>\n<link href=\"/stylesheets/too_big_stylesheet_split2.css\" media=\"screen\" rel=\"stylesheet\" type=\"text/css\" />\n<![endif]-->", output
+      assert_equal "<link href=\"/stylesheets/too_big_stylesheet.css\" media=\"screen\" rel=\"stylesheet\" />\n<!--[if lte IE 9]>\n<link href=\"/stylesheets/too_big_stylesheet_split2.css\" media=\"screen\" rel=\"stylesheet\" />\n<![endif]-->", output
     end
 
     test "should work with options and multiple stylesheets" do
       output = split_stylesheet_link_tag("too_big_stylesheet", "foo", media: "print")
-      assert_equal "<link href=\"/stylesheets/too_big_stylesheet.css\" media=\"print\" rel=\"stylesheet\" type=\"text/css\" />\n<link href=\"/stylesheets/foo.css\" media=\"print\" rel=\"stylesheet\" type=\"text/css\" />\n<!--[if lte IE 9]>\n<link href=\"/stylesheets/too_big_stylesheet_split2.css\" media=\"print\" rel=\"stylesheet\" type=\"text/css\" />\n<link href=\"/stylesheets/foo_split2.css\" media=\"print\" rel=\"stylesheet\" type=\"text/css\" />\n<![endif]-->", output
+      assert_equal "<link href=\"/stylesheets/too_big_stylesheet.css\" media=\"print\" rel=\"stylesheet\" />\n<link href=\"/stylesheets/foo.css\" media=\"print\" rel=\"stylesheet\" />\n<!--[if lte IE 9]>\n<link href=\"/stylesheets/too_big_stylesheet_split2.css\" media=\"print\" rel=\"stylesheet\" />\n<link href=\"/stylesheets/foo_split2.css\" media=\"print\" rel=\"stylesheet\" />\n<![endif]-->", output
     end
 
   end
