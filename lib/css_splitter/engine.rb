88c99a933374a67312090f47e85e9a4d58d61362
Clyde Law <clyde@futureadvisor.com>
2014-05-05 16:08:23 -0700

    Convert splitter engine to be a Sprockets bundle processor.

--- lib/css_splitter/engine.rb
+++ lib/css_splitter/engine.rb
@@ -3,7 +3,7 @@ module CssSplitter
     isolate_namespace CssSplitter
 
     initializer 'css_splitter.sprockets_engine', after: 'sprockets.environment', group: :all do |app|
-      app.assets.register_engine '.split2', CssSplitter::SprocketsEngine
+      app.assets.register_bundle_processor 'text/css', CssSplitter::SprocketsEngine
     end
 
     initializer 'css_splitter.action_controller' do |app|
