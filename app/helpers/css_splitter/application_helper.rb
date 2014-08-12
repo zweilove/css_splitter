dd81ca04d5c2c6845fe9d634363efeb81c519843
Lind <r627099@TRG502895.local>
2014-08-12 11:13:07 -0700

    Updated helper to not piggyback off of original.

--- app/helpers/css_splitter/application_helper.rb
+++ app/helpers/css_splitter/application_helper.rb
@@ -1,23 +1,30 @@
 module CssSplitter
   module ApplicationHelper
     def split_stylesheet_link_tag(*sources)
-      if Rails.env != 'development'
-        split_stylesheet_link_tag(sources)
-      else
-        options     = sources.
-                      extract_options!.
-                      merge!({
-                        href: "/assets/#{ sources.first }.css",
-                        rel: :stylesheet
-                      })
-        split_count = options.delete(:split_count) || 2
+      options     = sources.extract_options!
+      split_count = options.delete(:split_count) || 2
+      sources.map do |source|
+        split_sources = (2..split_count).map { |index| "#{ source }_split#{ index }" }
 
-        sources.map do |source|
-          split_sources = (2..split_count).map { |index| "#{ source }_split#{ index }" }
-
+        if Rails.env != 'development'
           lines = [
-            content_tag(:link, nil, options),
             stylesheet_link_tag(source, options),
+            "<!--[if lte IE 9]>"
+          ]
+
+          split_sources.each do |split_source|
+            lines << stylesheet_link_tag(split_source, options)
+          end
+        else
+          lines = [
+            content_tag(
+              :link,
+              nil,
+              options.
+              merge!({
+                href: "/assets/#{ sources.first }.css",
+                rel: :stylesheet
+              })),
             "<!--[if lte IE 9]>"
           ]
 
@@ -28,10 +35,10 @@ module CssSplitter
                        options.merge({ href: "/assets/#{ split_source }.css" })
                      )
           end
-          lines << "<![endif]-->"
-          lines
-        end.flatten.join("\n").html_safe
-      end
+        end
+        lines << "<![endif]-->"
+        lines
+      end.flatten.join("\n").html_safe
     end
   end
 end
