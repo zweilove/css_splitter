7f2bd956590f6434835d2cc993a6480905cb11d6
Bartek Kruszczynski <bartek@codequest.com>
2014-11-27 00:21:40 +0100

    Test new conditional debug.

--- test/unit/helpers/css_splitter/application_helper_test.rb
+++ test/unit/helpers/css_splitter/application_helper_test.rb
@@ -18,5 +18,24 @@ module CssSplitter
       assert_equal "<link href=\"/stylesheets/too_big_stylesheet.css\" media=\"screen\" rel=\"stylesheet\" />\n<!--[if lte IE 9]>\n<link href=\"/stylesheets/too_big_stylesheet_split2.css\" media=\"screen\" rel=\"stylesheet\" />\n<link href=\"/stylesheets/too_big_stylesheet_split3.css\" media=\"screen\" rel=\"stylesheet\" />\n<![endif]-->", output
     end
 
+    test "should default to false on splits" do
+      Rails.env = 'development'
+      output = split_stylesheet_link_tag("too_big_stylesheet")
+      Rails.env = 'test'
+      assert_equal "<link href=\"/stylesheets/too_big_stylesheet.css\" media=\"screen\" rel=\"stylesheet\" />\n<!--[if lte IE 9]>\n<link debug=\"false\" href=\"/stylesheets/too_big_stylesheet_split2.css\" media=\"screen\" rel=\"stylesheet\" />\n<![endif]-->", output
+    end
+
+    test "should respect the debug=true option" do
+      Rails.env = 'development'
+      output = split_stylesheet_link_tag("too_big_stylesheet", debug: true)
+      Rails.env = 'test'
+      assert_equal "<link debug=\"true\" href=\"/stylesheets/too_big_stylesheet.css\" media=\"screen\" rel=\"stylesheet\" />\n<!--[if lte IE 9]>\n<link debug=\"true\" href=\"/stylesheets/too_big_stylesheet_split2.css\" media=\"screen\" rel=\"stylesheet\" />\n<![endif]-->", output
+    end
+    test "should respect the debug=false option" do
+      Rails.env = 'development'
+      output = split_stylesheet_link_tag("too_big_stylesheet", debug: false)
+      Rails.env = 'test'
+      assert_equal "<link debug=\"false\" href=\"/stylesheets/too_big_stylesheet.css\" media=\"screen\" rel=\"stylesheet\" />\n<!--[if lte IE 9]>\n<link debug=\"false\" href=\"/stylesheets/too_big_stylesheet_split2.css\" media=\"screen\" rel=\"stylesheet\" />\n<![endif]-->", output
+    end
   end
 end
