0d4651eb530e8559d4097c142110e96e7b45ebf8
Bartek Kruszczynski <bartek@codequest.com>
2014-11-27 10:46:50 +0100

    Group tests for better readability.

--- test/unit/helpers/css_splitter/application_helper_test.rb
+++ test/unit/helpers/css_splitter/application_helper_test.rb
@@ -18,24 +18,28 @@ module CssSplitter
       assert_equal "<link href=\"/stylesheets/too_big_stylesheet.css\" media=\"screen\" rel=\"stylesheet\" />\n<!--[if lte IE 9]>\n<link href=\"/stylesheets/too_big_stylesheet_split2.css\" media=\"screen\" rel=\"stylesheet\" />\n<link href=\"/stylesheets/too_big_stylesheet_split3.css\" media=\"screen\" rel=\"stylesheet\" />\n<![endif]-->", output
     end
 
-    test "should default to false on splits" do
-      Rails.env = 'development'
-      output = split_stylesheet_link_tag("too_big_stylesheet")
-      Rails.env = 'test'
-      assert_equal "<link href=\"/stylesheets/too_big_stylesheet.css\" media=\"screen\" rel=\"stylesheet\" />\n<!--[if lte IE 9]>\n<link debug=\"false\" href=\"/stylesheets/too_big_stylesheet_split2.css\" media=\"screen\" rel=\"stylesheet\" />\n<![endif]-->", output
-    end
+    class RailsEnvDefault < ActionView::TestCase
+      setup do
+        Rails.env = 'development'
+      end
 
-    test "should respect the debug=true option" do
-      Rails.env = 'development'
-      output = split_stylesheet_link_tag("too_big_stylesheet", debug: true)
-      Rails.env = 'test'
-      assert_equal "<link debug=\"true\" href=\"/stylesheets/too_big_stylesheet.css\" media=\"screen\" rel=\"stylesheet\" />\n<!--[if lte IE 9]>\n<link debug=\"true\" href=\"/stylesheets/too_big_stylesheet_split2.css\" media=\"screen\" rel=\"stylesheet\" />\n<![endif]-->", output
-    end
-    test "should respect the debug=false option" do
-      Rails.env = 'development'
-      output = split_stylesheet_link_tag("too_big_stylesheet", debug: false)
-      Rails.env = 'test'
-      assert_equal "<link debug=\"false\" href=\"/stylesheets/too_big_stylesheet.css\" media=\"screen\" rel=\"stylesheet\" />\n<!--[if lte IE 9]>\n<link debug=\"false\" href=\"/stylesheets/too_big_stylesheet_split2.css\" media=\"screen\" rel=\"stylesheet\" />\n<![endif]-->", output
+      teardown do
+        Rails.env = 'test'
+      end
+
+      test "should default to false on splits" do
+        output = split_stylesheet_link_tag("too_big_stylesheet")
+        assert_equal "<link href=\"/stylesheets/too_big_stylesheet.css\" media=\"screen\" rel=\"stylesheet\" />\n<!--[if lte IE 9]>\n<link debug=\"false\" href=\"/stylesheets/too_big_stylesheet_split2.css\" media=\"screen\" rel=\"stylesheet\" />\n<![endif]-->", output
+      end
+
+      test "should respect the debug=true option" do
+        output = split_stylesheet_link_tag("too_big_stylesheet", debug: true)
+        assert_equal "<link debug=\"true\" href=\"/stylesheets/too_big_stylesheet.css\" media=\"screen\" rel=\"stylesheet\" />\n<!--[if lte IE 9]>\n<link debug=\"true\" href=\"/stylesheets/too_big_stylesheet_split2.css\" media=\"screen\" rel=\"stylesheet\" />\n<![endif]-->", output
+      end
+      test "should respect the debug=false option" do
+        output = split_stylesheet_link_tag("too_big_stylesheet", debug: false)
+        assert_equal "<link debug=\"false\" href=\"/stylesheets/too_big_stylesheet.css\" media=\"screen\" rel=\"stylesheet\" />\n<!--[if lte IE 9]>\n<link debug=\"false\" href=\"/stylesheets/too_big_stylesheet_split2.css\" media=\"screen\" rel=\"stylesheet\" />\n<![endif]-->", output
+      end
     end
   end
 end
