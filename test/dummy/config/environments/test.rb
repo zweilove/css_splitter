0b319d09bea60346085980a97550ddd3c58d9af0
Ruben Swieringa <ruben.swieringa@nedap.com>
2015-08-29 12:44:09 +0200

    Set config.active_support.test_order to get rid of deprecation warning for Rails 5
    “DEPRECATION WARNING: You did not specify a value for the configuration option ‘active_support.test_order’. In Rails 5, the default value of this option will change from ‘:sorted’ to ‘:random’.
    To disable this warning and keep the current behavior, you can add the following line to your ‘config/environments/test.rb’:”

--- test/dummy/config/environments/test.rb
+++ test/dummy/config/environments/test.rb
@@ -29,6 +29,9 @@ Dummy::Application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
+  # run tests in a random order
+  config.active_support.test_order = :random
+
   # Raise exception on mass assignment protection for Active Record models
   # config.active_record.mass_assignment_sanitizer = :strict
 
