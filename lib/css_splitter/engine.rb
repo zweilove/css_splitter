45cb5bcf460afd0ed4ded6570e1b6d2a470312a1
achernyshev <AChernyshev@sphereconsultinginc.com>
2016-04-14 17:58:16 +0300

    fix sprockets engine registering for older sprocket-rails versions

--- lib/css_splitter/engine.rb
+++ lib/css_splitter/engine.rb
@@ -1,8 +1,12 @@
 module CssSplitter
   class Engine < ::Rails::Engine
     initializer 'css_splitter.sprockets_engine', after: 'sprockets.environment', group: :all do |app|
-      app.config.assets.configure do |assets|
-        assets.register_bundle_processor 'text/css', CssSplitter::SprocketsEngine
+      if app.config.assets.public_methods.include? :configure
+        app.config.assets.configure do |assets|
+          assets.register_bundle_processor 'text/css', CssSplitter::SprocketsEngine
+        end
+      else
+        app.assets.register_bundle_processor 'text/css', CssSplitter::SprocketsEngine
       end
     end
 
