2d4211cafce6b4077dd3b1fafcecee5733515d4c
Nick Donald <n.donald@celmatix.com>
2016-07-25 16:00:40 -0400

    Clarify syntax, add comments

--- lib/css_splitter/sprockets_engine.rb
+++ lib/css_splitter/sprockets_engine.rb
@@ -11,12 +11,19 @@ module CssSplitter
     end
 
     def self.call(input)
-      filename = input[:filename]
-      data     = input[:data]
-      context  = input[:environment].context_class.new(input)
+      data_in = input[:data]
 
-      data = self.new(filename) { data }.render(context, {})
-      context.metadata.merge(data: data.to_str)
+      # Instantiate Sprockets::Context to pass along helper methods for Tilt
+      # processors
+      context = input[:environment].context_class.new(input)
+
+      # Pass the asset file contents as a block to the template engine,
+      # then get the results of the engine rendering
+      engine = self.new { data_in }
+      rendered_data = engine.render(context, {})
+
+      # Return the data and any metadata (ie file dependencies, etc)
+      context.metadata.merge(data: rendered_data.to_str)
     end
 
     def evaluate(scope, locals, &block)
