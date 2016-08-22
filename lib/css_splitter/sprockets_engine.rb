24017c19c60739d2d1038e985687117839054150
Nick Donald <nickjdonald@gmail.com>
2016-08-22 06:13:32 -0400

    Follow sproket engine interface guidelines (#70)
    * Follow sproket engine interface guidelines
    * Clarify syntax, add comments

--- lib/css_splitter/sprockets_engine.rb
+++ lib/css_splitter/sprockets_engine.rb
@@ -10,6 +10,22 @@ module CssSplitter
     def prepare
     end
 
+    def self.call(input)
+      data_in = input[:data]
+
+      # Instantiate Sprockets::Context to pass along helper methods for Tilt
+      # processors
+      context = input[:environment].context_class.new(input)
+
+      # Pass the asset file contents as a block to the template engine,
+      # then get the results of the engine rendering
+      engine = self.new { data_in }
+      rendered_data = engine.render(context, {})
+
+      # Return the data and any metadata (ie file dependencies, etc)
+      context.metadata.merge(data: rendered_data.to_str)
+    end
+
     def evaluate(scope, locals, &block)
       # Evaluate the split if the asset is named with a trailing _split2, _split3, etc.
       if scope.logical_path =~ /_split(\d+)$/
